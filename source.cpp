10fc854e9c03e294ee1be04b7883a88f26a94134
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-08-10 13:09:31 +0200

    Already done solution for Inherited-Code problem

--- source.cpp
+++ source.cpp
@@ -1,6 +1,50 @@
 ﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <exception>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class BadLengthException {
+public: BadLengthException(int n) : n(n) {}
+	  int what() const
+	  {
+		  return n;
+	  }
+private:
+	int n;
+};
+
+
+bool checkUsername(std::string username) {
+	bool isValid = true;
+	int n = username.length();
+	if (n < 5) {
+		throw BadLengthException(n);
+	}
+	for (int i = 0; i < n - 1; i++) {
+		if (username[i] == 'w' && username[i + 1] == 'w') {
+			isValid = false;
+		}
+	}
+	return isValid;
+}
+
+int main() {
+	int T; std::cin >> T;
+	while (T--) {
+		std::string username;
+		std::cin >> username;
+		try {
+			bool isValid = checkUsername(username);
+			if (isValid) {
+				std::cout << "Valid" << '\n';
+			}
+			else {
+				std::cout << "Invalid" << '\n';
+			}
+		}
+		catch (BadLengthException& e) {
+			std::cout << "Too short: " << e.what() << '\n';
+		}
+	}
+	return 0;
 }
